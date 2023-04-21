58f7727b0304346d572643c63ee9ede88d1e1a6b
stark1269 <stark1269@gmail.com>
2023-04-21 13:01:56 +0300

    Update ContactList.styled.jsx

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const List = styled.ul`
 overflow: auto;
 padding: 8px;
-max-height: 240px;
+max-height: 215px;
 margin-top: 18px;
 display: flex;
 flex-direction: column;
