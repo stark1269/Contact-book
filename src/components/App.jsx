f7fe4d60de9c8db6e3d4ace1fb504be68e6de7f1
stark1269 <stark1269@gmail.com>
2023-04-27 13:20:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,18 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({
+    contacts,
+  })};
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
+
   addContact = (newContact, name) => {
     if (this.state.contacts.some(value => value.name.toLocaleLowerCase() === name.toLocaleLowerCase())) {
       Notiflix.Notify.failure(`${name} is already in contacts!`);
