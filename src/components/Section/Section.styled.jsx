5f4b0a69ef78644518302dbf44710fbfaa70d332
stark1269 <stark1269@gmail.com>
2023-06-08 16:41:51 +0300

    Update Section.styled.jsx

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
+export const Title = styled.h2`
 font-size: 42px;
 margin-bottom: 18px;
 `;
 
-export const AfterTitle = styled.h1`
+export const AfterTitle = styled.h2`
 font-size: 32px;
 margin-bottom: 18px;
 `;
